64b4fea0132514abc83f65da5ec77494ddf6cd25
UZ9 <ryderjohnsonsh@gmail.com>
2024-08-29 15:32:49 -0400

    Add cmake server to lspconfig

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -2,8 +2,8 @@
 local M = {}
 
 M.ui = {
-  theme = "nightfox",
-  theme_toggle = { "nightfox", "one_light" },
+  theme = "everforest",
+  theme_toggle = { "everforest", "one_light" },
   lsp_semantic_tokens = true,
   statusline = {
     theme = "vscode_colored",
--- nvim/lua/plugins/spec/lspconfig.lua
+++ nvim/lua/plugins/spec/lspconfig.lua
@@ -10,7 +10,7 @@ return {
     local capabilities = require("nvchad.configs.lspconfig").capabilities
 
     local lspconfig = require "lspconfig"
-    local servers = { "html", "cssls", "jdtls", "tsserver", "yamlls", "clangd" }
+    local servers = { "html", "cssls", "jdtls", "tsserver", "yamlls", "clangd", "cmake" }
 
     -- lsps with default config
     for _, lsp in ipairs(servers) do
