c87d4cbda58ee473982e3be696a942356e23cdce
Yerti <36551149+UZ9@users.noreply.github.com>
2024-10-09 21:14:01 -0400

    Fix sessionizer newline error

--- .local/bin/scripts/tmux-project-manager.sh
+++ .local/bin/scripts/tmux-project-manager.sh
@@ -14,6 +14,8 @@ fi
 
 selected_name=$(basename "$selected" | tr . _)
 
+echo $selected_name
+
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then 
@@ -21,8 +23,9 @@ if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
   exit 0
 fi 
 
+echo $selected_name
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
   tmux new-session -ds $selected_name -c $selected
-for_in 
+fi 
 
 tmux switch-client -t $selected_name
