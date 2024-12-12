b3402ce547e724e2f92d8a4fd2cf0d1c4e0e9067
Yerti <36551149+UZ9@users.noreply.github.com>
2024-12-12 18:28:48 -0500

    Add hidden files to oil

--- nvim/lua/plugins/spec/oil.lua
+++ nvim/lua/plugins/spec/oil.lua
@@ -3,7 +3,12 @@ return {
   "stevearc/oil.nvim",
   lazy = false,
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true
+      }
+
+    })
 
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
