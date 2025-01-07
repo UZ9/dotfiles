7a90e8a3bdbbb0ec2504aa0d3edc46327c5d2145
UZ9 <ryderjohnsonsh@gmail.com>
2025-01-07 08:01:41 -0500

    Add dap mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -21,7 +21,7 @@ end
 map("n", "<C-d>", execute_lazy "<C-d>zz", { desc = "Move down half a page and center" })
 map("n", "<C-b>", execute_lazy "<C-b>zz", { desc = "Move up half a page and center" })
 
--- Quickly rearrange lines 
+-- Quickly rearrange lines
 map("n", "<C-k>", ":m-2<CR>")
 map("n", "<C-j>", ":m+1<CR>")
 
@@ -31,5 +31,26 @@ map("v", ">", ">gv")
 vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "<Tab>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
--- Compile cmake 
+-- Compile cmake
 map("n", "<leader>rr", "<cmd>make -C build<cr>", { desc = "Compile CMake project" })
+
+-- nvim-dap
+vim.keymap.set("n", "<Leader>b", function()
+  require("dap").toggle_breakpoint()
+end)
+
+vim.keymap.set("n", "<F5>", function()
+  require("dap").continue()
+end)
+
+vim.keymap.set("n", "<Leader>dl", function()
+  require("dap").run_last()
+end)
+vim.keymap.set("n", "<Leader>df", function()
+  local widgets = require "dap.ui.widgets"
+  widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set("n", "<Leader>ds", function()
+  local widgets = require "dap.ui.widgets"
+  widgets.centered_float(widgets.scopes)
+end)
