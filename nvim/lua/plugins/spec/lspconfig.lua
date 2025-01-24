79f27d433862503135c6f88fbd03e447a2c13d0a
Yerti <36551149+UZ9@users.noreply.github.com>
2025-01-24 14:50:32 -0500

    Add CloudFormation linting

--- nvim/lua/plugins/spec/lspconfig.lua
+++ nvim/lua/plugins/spec/lspconfig.lua
@@ -10,7 +10,7 @@ return {
     local capabilities = require("nvchad.configs.lspconfig").capabilities
 
     local lspconfig = require "lspconfig"
-    local servers = { "html", "cssls", "jdtls", "tsserver", "yamlls", "clangd", "cmake", "bashls", }
+    local servers = { "html", "cssls", "jdtls", "tsserver", "clangd", "cmake", "bashls" }
 
     -- lsps with default config
     for _, lsp in ipairs(servers) do
@@ -36,24 +36,48 @@ return {
       on_attach = on_attach,
       on_init = on_init,
       capabilities = capabilities,
-      filetypes = { "sv", "systemverilog" }
+      filetypes = { "sv", "systemverilog" },
     }
 
     lspconfig.yamlls.setup {
-      lspconfig.yamlls.setup {
-        on_attach = function(client, bufnr)
-          client.server_capabilities.documentFormattingProvider = true
-          on_attach(client, bufnr)
-        end,
-        capabilities = capabilities,
-        settings = {
-          yaml = {
-            format = {
-              enable = true,
-            },
-            schemaStore = {
-              enable = true,
-            },
+      on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = true
+        on_attach(client, bufnr)
+      end,
+      capabilities = capabilities,
+      settings = {
+        yaml = {
+          schemas = {
+            ["https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json"] = "/*",
+          },
+          format = {
+            enable = true,
+          },
+          hover = true,
+          completion = true,
+          schemaStore = {
+            enable = true,
+          },
+
+          customTags = {
+            "!fn",
+            "!And",
+            "!If",
+            "!Not",
+            "!Equals",
+            "!Or",
+            "!FindInMap sequence",
+            "!Base64",
+            "!Cidr",
+            "!Ref",
+            "!Ref Scalar",
+            "!Sub",
+            "!GetAtt",
+            "!GetAZs",
+            "!ImportValue",
+            "!Select",
+            "!Split",
+            "!Join sequence",
           },
         },
       },
