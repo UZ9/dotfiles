2451f28ae382c6593d6cf79ea7378f5692456941
Yerti <36551149+UZ9@users.noreply.github.com>
2026-09-22 16:35:49 -0400

    nit: force tsupdate with treesitter

--- nvim/lua/plugins/spec/treesitter.lua
+++ nvim/lua/plugins/spec/treesitter.lua
@@ -1,17 +1,12 @@
 --- @type LazyPluginSpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  config = function(_, opts)
-    local preinstalled_langs = {
-      ensure_installed = { "bash", "python", "lua", "yaml", "json", "toml" },
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter").setup {
+      install_dir = vim.fn.stdpath("data") .. "/site",
     }
-
-    -- avoid conflicting with original config
-    local final_opts = vim.tbl_deep_extend("force", opts, preinstalled_langs)
-
-    -- require("nvim-treesitter.configs").setup(final_opts)
-
     vim.treesitter.language.register("markdown", "mdx")
   end,
 }
